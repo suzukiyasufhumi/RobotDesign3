eeca7fc5803ab17526eca72a7ed2febd6fc81d85
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-05-07 19:27:02 +0000

    Remove a redundant file

--- setup.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -exv
-
-sudo chmod o-t /run/shm/
-
-sudo apt-get install apache2
-sudo cp ./apache2.conf /etc/apache2/sites-available/default
-sudo service apache2 restart
-sudo make install
