2591b8fdf41fec7383bdda404c64f470ce0bf041
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-10-31 09:57:18 +0000

    Fix some bugs

--- cgi/up_and_run.bash
+++ cgi/up_and_run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 
-exec 2> /tmp/up_and_set.bash
+exec 2> /tmp/up_and_run.bash
 
 tmp=/tmp/$$
 
@@ -8,12 +8,15 @@ tmp=/tmp/$$
 dd bs=${CONTENT_LENGTH}	count=1	|
 nkf -wLux > $tmp-upload
 
+cat $tmp-upload > /tmp/aho
+
 ###ファイル名を取り出す###
 FILE=$(head -n 3 $tmp-upload | grep -o 'filename="[^"]*"' | sed 's/[^"]*"//' | sed 's/".*//')
 echo $FILE > /tmp/boke
 
-sed '1,/^$/s/^/\/\//' $tmp-upload	|
-sed '$s/^/\/\//'	> $tmp-contents
+sed -n '/^$/,$p' $tmp-upload	|
+tail -n +2 			|
+grep -v '^----'			> $tmp-contents
 
 ###ファイル形式ごとの処理###
 case "$FILE" in
@@ -23,16 +26,19 @@ case "$FILE" in
 	g++ $tmp-main.cc -lm -o $tmp-main
 	chmod +x $tmp-main
 	#実行したいファイルの名前を/tmp/executableに入れる
-	echo $tmp-main > /tmp/executable
+	mv $tmp-main /tmp/executable
+;;
 *.c )
 	mv $tmp-contents $tmp-main.c
 
 	gcc $tmp-main.cc -lm -o $tmp-main
 	chmod +x $tmp-main
 	#実行したいファイルの名前を/tmp/executableに入れる
-	echo $tmp-main > /tmp/executable
+	mv $tmp-main /tmp/executable
+;;
 *)
-	mv $tmp-contents /tmp/executable
+	cat $tmp-contents > /tmp/executable
+;;
 esac
 
 chmod a+x /tmp/executable
